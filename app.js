ab2a40b3df655f52fbbb3f6b97423ffcc0d3cecd
ammkutti2310 <naiduiitkgp21@gmail.com>
2022-03-25 13:29:26 +0000

    API 8 is added:Returns an object containing the state name of a district based on the district ID

--- app.js
+++ app.js
@@ -67,3 +67,98 @@ VALUES
     `${districtName} district(state_id ${stateId}) is seccessfully added `
   );
 });
+
+//API 4: Returns a district based on the district ID
+app.get("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+  const getDistrictQuery = `SELECT * FROM district WHERE district_id = ${districtId};`;
+  const districtDetails = await db.get(getDistrictQuery);
+  console.log(districtDetails);
+  const {
+    district_id,
+    district_name,
+    state_id,
+    cases,
+    cured,
+    active,
+    deaths,
+  } = districtDetails;
+
+  response.send({
+    districtId: district_id,
+    districtName: district_name,
+    stateId: state_id,
+    cases: cases,
+    cured: cured,
+    active: active,
+    deaths: deaths,
+  });
+  console.log(`Returns a district based on the district ID ${district_id}`);
+});
+
+//API 5: Deletes a district from the district table based on the district ID
+app.get("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+  const deleteDistrictQuery = `DELETE FROM district WHERE district_id = ${districtId};`;
+  await db.run(deleteDistrictQuery);
+  response.send("District Removed");
+  console.log(
+    `Deletes a district from the district table based on the district ID ${districtId}`
+  );
+});
+
+//API 6: Updates the details of a specific district based on the district ID
+app.put("/districts/:districtId/", async (request, response) => {
+  const { districtId } = request.params;
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const updateDistrictQuery = `UPDATE district
+SET 
+district_name = '${districtName}',
+state_id = ${stateId},
+cases = ${cases}, 
+cured = ${cured},
+active =  ${active} , 
+deaths =  ${deaths}
+WHERE district_id = ${districtId};`;
+  await db.run(updateDistrictQuery);
+  response.send("District Details Updated");
+  console.log(request.body);
+  console.log(`District(district_id ${districtId}) Details Updated`);
+});
+
+//API 7: Returns the statistics of total cases, cured, active, deaths of a specific state based on state ID
+app.get("/states/:stateId/stats/", async (request, response) => {
+  const { stateId } = request.params;
+  const getStatsQuery = ` SELECT 
+SUM(cases) AS total_cases, 
+SUM(cured) AS cured,
+SUM(active) AS active,
+SUM(deaths) AS deaths
+FROM district WHERE state_id = ${stateId};`;
+
+  const stats = await db.get(getStatsQuery);
+  console.log(stats);
+  response.send({
+    totalCases: stats.total_cases,
+    totalCured: stats.cured,
+    totalActive: stats.active,
+    totalDeaths: stats.deaths,
+  });
+  console.log(`Returns the statistics of state_id ${stateId}`);
+});
+
+//API 8: Returns an object containing the state name of a district based on the district ID
+app.get("/districts/:districtId/details/", async (request, response) => {
+  const { districtId } = request.params;
+  const getStateNameQuery = `SELECT state_name FROM district 
+INNER JOIN state ON district.state_id = state.state_id
+WHERE district_id = ${districtId};`;
+  const stateName = await db.get(getStateNameQuery);
+  console.log(stateName);
+  response.send({
+    stateName: stateName.state_name,
+  });
+  console.log(
+    `Returned state name of a district based on the district ID ${districtId}`
+  );
+});
